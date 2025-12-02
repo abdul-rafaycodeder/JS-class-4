a19f71af6136a9e68bccff32ed3330538ff9c811
Abdul Rafay <abdulrafaycodeder@gmail.com>
2025-12-02 10:49:23 -0800

    JS-class-3

---
+++ index.js
@@ -0,0 +1,47 @@
+// var uname = "ali";
+// var age = 20;
+// var roll = 123;
+// var data = 50;
+
+// if ((uname == "ali") && (age == 20 || roll == 123) ) {
+//     console.log("true")
+// }
+
+// else {
+//     console.log("false")
+// }
+
+
+// var c = 2;
+// var d = 4;
+// var x = 5;
+// var y = 6;
+
+// var g = 1;
+// var h = 0;
+
+// if (c == d) {
+//     if (x == y) {
+
+//     }
+// }
+
+var uname = "ali";
+var day = "mon";
+
+
+if (uname == "ali") {
+
+    if (day == "mon") {
+        console.log("welcome")
+    
+    }
+    else {
+        console.log("aj app ki calss nahi hai ghar jao")
+    }
+}
+
+else{
+    console.log("ya app ka cumpuss nahi hai ")
+}
+
